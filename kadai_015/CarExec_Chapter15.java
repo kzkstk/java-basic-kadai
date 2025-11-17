8990012d7a00c91b523573f91d290157ecdacb46
kzkstk <kazuki-10.6.10@ezweb.ne.jp>
2025-11-17 22:42:12 +0900

    Add files via upload

--- kadai_015/CarExec_Chapter15.java
+++ kadai_015/CarExec_Chapter15.java
@@ -5,11 +5,10 @@ public class CarExec_Chapter15 {
 	public static void main(String[] args) {
 	
 	//ギアのデータを生成
-		Car_Chapter15 car1 = new Car_Chapter15(1,10,1);
+		Car_Chapter15 car1 = new Car_Chapter15(1,10);
 	
 	//車クラスのgearChangeメソッドを実行
-		car1.gearChange(3);
-		car1.run();
-		
+		car1.run(3);
+		car1.gearChange(5);		
 	}
 }

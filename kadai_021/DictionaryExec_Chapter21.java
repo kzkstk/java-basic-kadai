6fdab62cc6bfbc4053d8adc044ac8c4957f304e7
kzkstk <kazuki-10.6.10@ezweb.ne.jp>
2025-11-17 22:29:18 +0900

    Add files via upload

--- kadai_021/DictionaryExec_Chapter21.java
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -1,10 +1,16 @@
 package kadai_021;
 
-public class DictionaryExec_Chapter21 {
+public class DictionaryExec_Chapter21 extends Dictionary_Chapter21{
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		//辞書クラスをインスタンス化
+		Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();
+		
+		//調べる単語の配列を作成
+		String[] keyWords = {"apple","banana","grape","orange"};
+		
+		//配列の単語を辞書で調べる
+		dictionary.checkWords(keyWords);
+   }
 
 	}
-
-}

d7ed8f0be0a6f719db3ba52fc2102d6cbed04264
kzkstk <kazuki-10.6.10@ezweb.ne.jp>
2025-11-17 22:35:42 +0900

    Add files via upload

--- kadai_018/KatoIchiro_Chapter18.java
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -1,6 +1,6 @@
 package kadai_018;
 
-public class KatoHanako_Chapter18 extends Kato_Chapter18{
+public class KatoIchiro_Chapter18 extends Kato_Chapter18 {
 	//名前を表すフィールドをセットする
 		public void setGivenName(String givenName) {
 			this.givenName = givenName;
@@ -8,7 +8,6 @@ public class KatoHanako_Chapter18 extends Kato_Chapter18{
 	
 	//抽象メソッドに具体的な処理を記述する
 		void eachIntroduce() {
-			System.out.println("趣味は読書です");
+			System.out.println("好きな食べ物はリンゴです");
 		}
-
 }

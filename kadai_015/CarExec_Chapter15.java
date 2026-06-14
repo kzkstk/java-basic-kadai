fdf2ced19c8f681fc8b5a0a1b4025df8f1de826b
kzkstk <kazuki-10.6.10@ezweb.ne.jp>
2026-06-14 14:07:10 +0900

    Add files via upload

--- kadai_015/CarExec_Chapter15.java
+++ kadai_015/CarExec_Chapter15.java
@@ -5,10 +5,10 @@ public class CarExec_Chapter15 {
 	public static void main(String[] args) {
 	
 	//ギアのデータを生成
-		Car_Chapter15 car1 = new Car_Chapter15(1,10);
+		Car_Chapter15 car1 = new Car_Chapter15();
 	
 	//車クラスのgearChangeメソッドを実行
-		car1.run(3);
-		car1.gearChange(5);		
+		car1.gearChange(5);	
+		car1.run();	
 	}
 }
--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -6,20 +6,17 @@ public class Car_Chapter15 {
 	//フィールド（内部データ）
 	private int gear  = 1;  //1速から5速のギアを表す
 	private int speed = 10; //10~50のスピードが出せる
-
-	//コンストラクタ（初期化処理）
-	public  Car_Chapter15(int gear,int speed) {
-		
-	}
 	
 	//【メソッド run】最初のギア値を表示する
-	public void run(int gear) {
-		this.gear=gear;
+	public void run() {
+	 System.out.println("速度は時速" + this.speed + "kmです");
 	}
 	
 	//【メソッド gearChange】ギアの値により速度を変える
 	public void gearChange(int afterGear) {
-         speed = switch(afterGear){
+		System.out.println("ギア" + this.gear + "から" + afterGear + "に切り替えました");
+		 this.gear=afterGear;
+         this.speed = switch(this.gear){
 			case 1 -> 10;
 			case 2 -> 20;
 			case 3 -> 30;
@@ -27,8 +24,5 @@ public class Car_Chapter15 {
 			case 5 -> 50;
 			default -> 10;
 		};
-	
-		System.out.println("ギア" + gear + "から" + afterGear + "に切り替えました");
-		System.out.println("速度は時速" + speed + "kmです");
     }
 }
